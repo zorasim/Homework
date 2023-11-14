a9bcfb9375d966fab9d3387c9a3b706169b27ac5
Bunnypop <133989817+Bunnypop24@users.noreply.github.com>
2023-11-14 23:49:47 +0100

    domashna 1 java

---
+++ homework java/hw1/ifelsehw.js
@@ -0,0 +1,66 @@
+//fist
+var number=15
+if(number%2==0){
+    console.log(number+" is even")
+}else{
+    console.log(number+" is not even")
+}
+//second
+var name1="Zora"
+var name2="Ati"
+if(name1==name2){
+    console.log("User one and two are:"+name1)
+}else{
+    console.log("User one is:"+name1+", while user two is:"+name2)
+}
+//third
+var number1="5"
+var number2=5
+if(number1==number2){
+    console.log("Number 1 is the same as number 2")
+}else{
+    console.log("Number 1 and 2 are diffrent")
+}
+//fourth
+if(number1===number2){
+    console.log("Number 1 and 2 are of the same type")
+}else{
+    console.log("Number 1("+number1+") type is: " +typeof name1 +". Number 2("+number2+") type is: "+typeof name2)
+}
+//fifth
+number1=10
+number2=20
+if(number1>number2){
+    console.log(number1+" is bigger than "+number2)
+}else{
+    console.log(number2+" is bigger than "+number1)
+}
+//sixth
+number1*=number2
+if(number1%2==0){
+    console.log("Even number")
+}else{
+    console.log("Odd number")
+}
+//seventh
+if(true){
+    console.log("meow!")
+}
+//eighth
+var sound1= "meow!"
+if(sound1==="meow!"){
+    console.log("Sound is coming from a cat")
+}
+//ninth
+if(number1==sound1){
+    console.log("Is this the twilight zone")
+}else{
+    console.log("a number isnt a cat!")
+}
+//tenth
+if(number1==number2){
+    console.log("how is this possible")
+}else{
+    console.log("You changed them remember")
+}
+//math
